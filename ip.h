b103f2eb665729f4e1bcaa6f181f4a735f88eab3
Kaisei <me@kaisei.dev>
2023-08-29 15:56:20 +0900

    done: step6

--- ip.h
+++ ip.h
@@ -1,5 +1,31 @@
 #pragma once
-#include <string.h>
+
+#include <stddef.h>
+#include <stddef.h>
+
+
+#define IP_VERSION_IPV4 4
+
+#define IP_HDR_SIZE_MIN 20
+#define IP_HDR_SIZE_MAX 60
+
+#define IP_TOTAL_SIZE_MAX UINT16_MAX
+#define IP_PAYLOAD_SIZE_MAX (IP_TOTAL_SIZE_MAX - IP_HDR_SIZE_MIN)
+
+#define IP_ADDR_LEN 4
+#define IP_ADDR_STR_LEN (IP_ADDR_LEN * 4)
+
+typedef uint32_t ip_addr_t;
+
+extern const ip_addr_t IP_ADDR_ANY;
+extern const ip_addr_t IP_ADDR_BROADCAST;
+
 
 static void ip_input(const uint8_t* , size_t, struct net_device*);
-int ip_init(void);
+
+extern int
+ip_addr_pton(const char *p, ip_addr_t *n);
+extern char *
+ip_addr_ntop(const ip_addr_t n, char *p, size_t size);
+
+extern int ip_init(void);
