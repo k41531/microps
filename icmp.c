1a96f6a03b87f70d9cfd5e9f2ab1ca405247f41f
Kaisei <me@kaisei.dev>
2023-08-29 22:13:37 +0900

    done: step10

--- icmp.c
+++ icmp.c
@@ -5,14 +5,100 @@
 #include "ip.h"
 #include "icmp.h"
 
+struct icmp_hdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint32_t values;
+};
+
+struct icmp_echo {
+    uint8_t type;
+    uint8_t code;
+    uint16_t sum;
+    uint16_t id;
+    uint16_t seq;
+};
+
+static char *
+icmp_type_ntoa(uint8_t type) {
+    switch(type){
+        case ICMP_TYPE_ECHOREPLY:
+            return "Echo Reply";
+        case ICMP_TYPE_DEST_UNREACH:
+            return "Destination Unreachable";
+        case IICMP_TYPE_SOURCE_QUENCH:
+            return "Source Quench";
+        case ICMP_TYPE_REDIRECT:
+            return "Redirect";
+        case ICMP_TYPE_ECHO:
+            return "Echo"; 
+        case ICMP_TYPE_TIME_EXCEEDED:
+            return "Time Exceeded";
+        case ICMP_TYPE_PARAM_PROBLEM:
+            return "Parameter Problem";
+        case ICMP_TYPE_TIMESTAMP:
+            return "Timestamp";
+        case ICMP_TYPE_TIMESTAMPREPLY:
+            return "Timestamp Reply";
+        case ICMP_TYPE_INFO_REQUEST:
+            return "Information Request";
+        case ICMP_TYPE_INFO_REPLY:
+            return "Information Reply";
+    }
+    return "Unknown";
+}
+
+static void
+icmp_dump(const uint8_t *data, size_t len)
+{
+    struct icmp_hdr *hdr;
+    struct icmp_echo *echo;
+
+    flockfile(stderr);
+    hdr = (struct icmp_hdr *)data;
+    fprintf(stderr, "       type: %u (%s)\n", hdr->type, icmp_type_ntoa(hdr->type));
+    fprintf(stderr, "       code: %u\n", hdr->code);
+    fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+    switch (hdr->type)
+    {
+    case ICMP_TYPE_ECHO:
+    case ICMP_TYPE_ECHOREPLY:
+        echo = (struct icmp_echo *)data;
+        fprintf(stderr, "          id: %u\n", ntoh16(echo->id));
+        fprintf(stderr, "         seq: %u\n", ntoh16(echo->seq));
+        break;
+    default:
+        fprintf(stderr, "       values: 0x%08x\n" , ntoh32(hdr->values));
+        break;
+    }
+#ifdef HEXDUMP
+    hexdump(stderr, data, len);
+#endif
+    funlockfile(stderr);
+}
+
+
 void
 icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface)
 {
+    struct icmp_hdr *hdr;
     char addr1[IP_ADDR_STR_LEN];
     char addr2[IP_ADDR_STR_LEN];
+    
+    // Chek ICMP message
+    if(len < sizeof(*hdr)){
+        errorf("too short");
+        return;
+    }
+    hdr = (struct icmp_hdr *)data;
+    if(cksum16((uint16_t *)data, len, 0) != 0){
+        errorf("checksum error, sum=0x%04x, verify=0x%04x", ntoh16(hdr->sum), ntoh16(cksum16((uint16_t *)data, len, -hdr->sum))); 
+        return;
+    }
 
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
-    debugdump(data, len);
+    icmp_dump(data, len);
 }
 
 int
