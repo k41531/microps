55af098f39c252cff970b737d43c6d1e45d71839
Kaisei <me@kaisei.dev>
2023-09-04 16:35:00 +0900

    done: step22

--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include "ip.h"
 #include "icmp.h"
 #include "udp.h"
+#include "tcp.h"
 
 struct net_protocol {
     struct net_protocol *next;
@@ -280,6 +281,10 @@ net_init(void)
     }
     if(udp_init() == -1){
         errorf("udp_init() failure");
+        return -1;
+    }
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
         return -1;
     }
     infof("initializied");
